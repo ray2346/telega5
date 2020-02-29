8a121240b46f5050ca6ca3dd734fc99f1bad3e44
ray2346 <54332835+ray2346@users.noreply.github.com>
2020-03-01 00:28:13 +0300

    commit7

--- index2.js
+++ index2.js
@@ -1,34 +1,8 @@
-const Telegraf = require('telegraf')
-const Markup = require('telegraf/markup')
-let TelegramBot = require('node-telegram-bot-api');
 
-const request = require('request');
+const telegramBot = require('node-telegram-bot-api')
+const token = '1084830404:AAEszzF1u3d8675TSh9qoFKJ_zWdRVADxdk';
+const  bot = new TelegramBot(token, {polling: true});
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '979957847:AAHysdp4haGKUFs9YiW0DG784p9P5Km-2Ss';
-
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+bot.on('message',msg =>{
+  bot.sendMessage(msg.chat.id,'Helloy heroku, bot saya: "hi , ${msg.from.first_name}"')
+})
