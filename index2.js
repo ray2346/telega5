1e84e4a1b3417e37bd7042b4b4cc560dd5c6cb91
ray2346 <54332835+ray2346@users.noreply.github.com>
2020-02-29 23:15:37 +0300

    commit4

--- index2.js
+++ index2.js
@@ -7,54 +7,28 @@ const request = require('request');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '979957847:AAHysdp4haGKUFs9YiW0DG784p9P5Km-2Ss';
 
+
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/curse/, (msg, match) => {
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
   const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
-  bot.sendMessage(chatId, 'Выберите какая валюта вас интересует', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: '€ - EUR',
-            callback_data: 'EUR'
-          }, {
-            text: '$ - USD',
-            callback_data: 'USD'
-          }, {
-            text: '₽ - RUR',
-            callback_data: 'RUR'
-          }, {
-            text: '₿ - BTC',
-            callback_data: 'BTC'
-          }
-        ]
-      ]
-    }
-  });
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
-bot.on('callback_query', query => {
-  const id = query.message.chat.id;
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-    const data = JSON.parse(body);
-    const result = data.filter(item => item.ccy === query.data)[0];
-    const flag = {
-      'EUR': '🇪🇺',
-      'USD': '🇺🇸',
-      'RUR': '🇷🇺',
-      'UAH': '🇺🇦',
-      'BTC': '₿'
-    }
-    let md = `
-      *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
-      Buy: _${result.buy}_
-      Sale: _${result.sale}_
-    `;
-    bot.sendMessage(id, md, {parse_mode: 'Markdown'});
-  })
-})
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
